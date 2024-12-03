f43fd35e534b0a4b45750d6fa91c0054ecd9c023
gatmbarz123 <bar30301@gmail.com>
2024-12-03 17:59:42 +0200

    Add Jenkins

--- vars/getNodeName.groovy
+++ vars/getNodeName.groovy
@@ -1,7 +1,5 @@
-def call() {
+def getNodeName() {
     def podName = sh(script: 'hostname', returnStdout: true).trim()
-    def nodeName = sh(script: """
-        kubectl get pod ${podName} -o jsonpath='{.spec.nodeName}'
-    """, returnStdout: true).trim()
+    def nodeName = sh(script: """kubectl get pod ${podName} -o jsonpath='{.spec.nodeName}'""", returnStdout: true).trim()
     return nodeName
 }
